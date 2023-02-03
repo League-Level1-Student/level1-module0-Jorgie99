b5fc3f5705dcead7e61ea3808a893235bd1159b0
League <kierancronin213@gmail.com>
2023-02-02 16:58:22 -0800

    Feb 2 second commit

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,40 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
-		
+	Robot rob = new Robot();
 	public void run() {
-		// Check the recipe to find out what code to put here
+		rob.setSpeed(20);
+		rob.moveTo(0,550);
+		rob.moveTo(700,300);
+		rob.turn(-90);
+		drawHouse("small");
+		drawHouse("medium");
+	}
+	public void drawHouse(String size) {
+		int height = 0;
+		if(size == "small") {
+			height = 100;
+		} else if(size == "medium") {
+			height = 200;
+		} else if(size == "large") {
+			height = 250;
+		}
+		rob.penDown();
+		rob.setPenColor(49,196,60);
+		rob.move(30);
+		rob.turn(90);
+		rob.setRandomPenColor();
+		rob.move(height);
+		rob.turn(-90);
+		rob.move(30);
+		rob.turn(-90);
+		rob.move(height);
+		rob.turn(90);
 	}
 }
